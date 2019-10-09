553d9f4f93d657fc92066c65590eb0a70c1979d4
cuizy <850043648@qq.com>
2019-10-09 19:47:11 +0800

    字典mapper增加注解

--- src/main/java/cn/com/wanwei/bic/mapper/DictionaryMapper.java
+++ src/main/java/cn/com/wanwei/bic/mapper/DictionaryMapper.java
@@ -1,8 +1,11 @@
 package cn.com.wanwei.bic.mapper;
 
 import cn.com.wanwei.bic.entity.DictionaryEntity;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface DictionaryMapper {
+
     int deleteByPrimaryKey(Long id);
 
     int insert(DictionaryEntity record);
