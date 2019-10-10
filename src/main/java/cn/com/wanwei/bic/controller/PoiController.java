4a3ef5c3e85eea634eb6f332794557a46ee8fa7f
e <1398975391@qq.com>
2019-10-10 17:27:29 +0800

    景点和poi合并

---
+++ src/main/java/cn/com/wanwei/bic/controller/PoiController.java
@@ -0,0 +1,110 @@
+package cn.com.wanwei.bic.controller;
+
+import cn.com.wanwei.bic.entity.PoiEntity;
+import cn.com.wanwei.bic.service.PoiService;
+import cn.com.wanwei.common.log.annotation.OperationLog;
+import cn.com.wanwei.common.model.ResponseMessage;
+import cn.com.wanwei.common.utils.RequestUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Slf4j
+@RefreshScope
+@RestController
+@RequestMapping("/poi")
+@Api(value = "poi管理", tags = "poi管理相关接口")
+public class PoiController extends  BaseController{
+
+    @Autowired
+    private PoiService poiService;
+
+    @ApiOperation(value = "poi管理分页列表",notes = "poi管理分页列表")
+    @GetMapping(value = "/page")
+    @PreAuthorize("hasAuthority('poi:r')")
+    @OperationLog(value = "wtcp-bics/poi管理分页列表", operate = "r", module = "poi管理")
+    public ResponseMessage findByPage(@RequestParam(value = "page", defaultValue = "1") Integer page,
+                                      @RequestParam(value = "size", defaultValue = "10") Integer size,
+                                      HttpServletRequest request){
+        Map<String, Object> filter = RequestUtil.getParameters(request);
+        return poiService.findByPage(page,size,filter);
+    }
+
+    @ApiOperation(value = "查询poi信息", notes = "根据ID查询poi信息")
+    @ApiImplicitParam(name = "id", value = "poi信息ID", required = true)
+    @GetMapping(value = "/{id}")
+    @PreAuthorize("hasAuthority('poi:r')")
+    @OperationLog(value = "wtcp-bics/查询poi信息", operate = "r", module = "poi管理")
+    public ResponseMessage find(@PathVariable("id") String id) {
+        return poiService.find(id);
+    }
+
+    @ApiOperation(value = "新增poi信息", notes = "新增poi信息")
+    @ApiImplicitParam(name = "poiEntity", value = "poi管理实体",dataType="PoiEntity", required = true)
+    @PostMapping
+    @PreAuthorize("hasAuthority('poi:c')")
+    @OperationLog(value = "wtcp-bics/新增poi信息", operate = "c", module = "poi管理")
+    public ResponseMessage create(@RequestBody PoiEntity poiEntity, BindingResult bindingResult) throws Exception {
+       if(bindingResult.hasErrors()){
+           return ResponseMessage.validFailResponse().setMsg(bindingResult.getAllErrors());
+       }
+        return poiService.create(poiEntity,getCurrentUser());
+    }
+
+    @ApiOperation(value = "编辑poi信息", notes = "编辑poi信息")
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "id", value = "poi信息ID", required = true),
+        @ApiImplicitParam(name = "poiEntity", value = "poi管理实体",dataType="PoiEntity", required = true)
+    })
+    @PutMapping(value = "/{id}")
+    @PreAuthorize("hasAuthority('poi:u')")
+    @OperationLog(value = "wtcp-bics/编辑poi信息", operate = "u", module = "poi管理")
+    public ResponseMessage edit(@PathVariable(value = "id") String id, @RequestBody PoiEntity poiEntity, BindingResult bindingResult) throws Exception {
+        if(bindingResult.hasErrors()){
+            return ResponseMessage.validFailResponse().setMsg(bindingResult.getAllErrors());
+        }
+        return poiService.update(id,poiEntity,getCurrentUser());
+    }
+
+    @ApiOperation(value = "删除poi信息", notes = "删除poi信息")
+    @ApiImplicitParam(name = "id", value = "poi信息ID", required = true)
+    @DeleteMapping(value = "/{id}")
+    @PreAuthorize("hasAuthority('poi:d')")
+    @OperationLog(value = "wtcp-bics/删除poi信息", operate = "d", module = "poi管理")
+    public ResponseMessage delete(@PathVariable(value = "id") String id){
+        return poiService.delete(id);
+    }
+
+    @ApiOperation(value = "权重更改", notes = "权重更改")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "poi信息ID", required = true),
+            @ApiImplicitParam(name = "weight", value = "权重", required = true)
+    })
+    @GetMapping(value = "weight/{id}")
+    @PreAuthorize("hasAuthority('poi:q')")
+    @OperationLog(value = "wtcp-bics/权重更改", operate = "u", module = "poi管理")
+    public ResponseMessage goWeight(@PathVariable(value = "id") String id,@RequestParam Float weight) throws Exception {
+        return poiService.goWeight(id,weight,getCurrentUser());
+    }
+
+    @ApiOperation(value = "标题重名校验", notes = "标题重名校验")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "poi信息ID"),
+            @ApiImplicitParam(name = "title", value = "标题")
+    })
+    @GetMapping(value = "checkTitle/{id}")
+    public ResponseMessage checkTitle(@PathVariable(value = "id",required = false) String id,
+                                      @RequestParam(value = "title",required = false) String title){
+        return poiService.checkTitle(id,title);
+    }
+}
---
+++ src/main/java/cn/com/wanwei/bic/service/impl/PoiServiceImpl.java
@@ -0,0 +1,163 @@
+package cn.com.wanwei.bic.service.impl;
+
+import cn.com.wanwei.bic.entity.PoiEntity;
+import cn.com.wanwei.bic.feign.CoderServiceFeign;
+import cn.com.wanwei.bic.mapper.PoiMapper;
+import cn.com.wanwei.bic.service.PoiService;
+import cn.com.wanwei.common.model.ResponseMessage;
+import cn.com.wanwei.common.model.User;
+import cn.com.wanwei.persistence.mybatis.MybatisPageRequest;
+import cn.com.wanwei.persistence.mybatis.PageInfo;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * wtcp-bics - PoiServiceImpl poi管理接口实现类
+ */
+@Service
+@Slf4j
+@RefreshScope
+public class PoiServiceImpl implements PoiService {
+
+    @Autowired
+    private PoiMapper poiMapper;
+
+    @Autowired
+    private CoderServiceFeign coderServiceFeign;
+
+    @Value("${wtcp.bic.appCode}")
+    protected Integer appId;
+
+    @Override
+    public ResponseMessage findByPage(Integer page, Integer size, Map<String, Object> filter) {
+        try {
+            Sort sort = Sort.by(new Sort.Order[]{new Sort.Order(Sort.Direction.DESC, "weight"),new Sort.Order(Sort.Direction.DESC, "createdDate")});
+            MybatisPageRequest pageRequest = MybatisPageRequest.of(page, size, sort);
+            PageHelper.startPage(pageRequest.getPage(),pageRequest.getSize(),pageRequest.getOrders());
+            Page<PoiEntity> poiEntities = poiMapper.findByPage(filter);
+            PageInfo<PoiEntity> pageInfo = new PageInfo<>(poiEntities, pageRequest);
+            return ResponseMessage.defaultResponse().setData(pageInfo);
+        } catch (Exception e) {
+            log.info(e.getMessage());
+            return ResponseMessage.validFailResponse().setMsg("获取失败！");
+        }
+    }
+
+    @Override
+    public ResponseMessage find(String id) {
+        try {
+            PoiEntity poiEntity =poiMapper.selectByPrimaryKey(id);
+            if(poiEntity==null){
+                return ResponseMessage.validFailResponse().setMsg("暂无该poi信息！");
+            }
+            return ResponseMessage.defaultResponse().setData(poiEntity);
+        } catch (Exception e) {
+           log.info(e.getMessage());
+            return ResponseMessage.validFailResponse().setMsg("获取失败！");
+        }
+    }
+
+    @Override
+    public ResponseMessage create(PoiEntity poiEntity, User user) {
+        try {
+            //获取统一认证生成的code
+            ResponseMessage responseMessageGetCode = coderServiceFeign.buildSerialNum(appId);
+            if(responseMessageGetCode.getStatus() == 1 && responseMessageGetCode.getData() != null) {
+                poiEntity.setCode(responseMessageGetCode.getData().toString());
+                poiEntity.setStatus(0);
+                poiEntity.setCreatedUser(user.getUsername());
+                poiEntity.setCreatedDate(new Date());
+                poiEntity.setDeptCode(user.getOrg().getCode());
+                poiMapper.insert(poiEntity);
+                return ResponseMessage.defaultResponse().setMsg("保存成功!");
+            }
+            return responseMessageGetCode;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseMessage.validFailResponse().setMsg("获取失败！");
+        }
+    }
+
+    @Override
+    public ResponseMessage update(String id, PoiEntity poiEntity, User user) {
+        try {
+            PoiEntity pEntity=poiMapper.selectByPrimaryKey(id);
+            if(pEntity!=null){
+                poiEntity.setCreatedUser(pEntity.getCreatedUser());
+                poiEntity.setCreatedDate(pEntity.getCreatedDate());
+                poiEntity.setStatus(0);
+                poiEntity.setCode(pEntity.getCode());
+                poiEntity.setDeptCode(user.getOrg().getCode());
+                poiEntity.setUpdatedUser(user.getUsername());
+                poiEntity.setUpdatedDate(new Date());
+                poiMapper.updateByPrimaryKey(poiEntity);
+                return ResponseMessage.defaultResponse().setMsg("更新成功！");
+            }
+            return ResponseMessage.validFailResponse().setMsg("暂无该poi信息！");
+        } catch (Exception e) {
+            log.info(e.getMessage());
+            return ResponseMessage.validFailResponse().setMsg("更新失败！");
+        }
+    }
+
+    @Override
+    public ResponseMessage delete(String id) {
+        try {
+            poiMapper.deleteByPrimaryKey(id);
+            return ResponseMessage.defaultResponse().setMsg("删除成功！");
+        } catch (Exception e) {
+           log.info(e.getMessage());
+            return ResponseMessage.validFailResponse().setMsg("删除失败！");
+        }
+    }
+
+    @Override
+    public ResponseMessage goWeight(String id, Float weight, User user) {
+        try {
+            PoiEntity pntity=poiMapper.selectByPrimaryKey(id);
+            if(pntity!=null){
+                pntity.setWeight(weight);
+                pntity.setDeptCode(user.getOrg().getCode());
+                pntity.setUpdatedUser(user.getUsername());
+                pntity.setUpdatedDate(new Date());
+                poiMapper.updateByPrimaryKey(pntity);
+                return ResponseMessage.defaultResponse().setMsg("权重修改成功！");
+            }
+            return ResponseMessage.validFailResponse().setMsg("暂无该poi信息！");
+        } catch (Exception e) {
+            log.info(e.getMessage());
+            return ResponseMessage.validFailResponse().setMsg("权重修改失败！");
+        }
+    }
+
+    @Override
+    public ResponseMessage checkTitle(String id, String title) {
+        ResponseMessage responseMessage=ResponseMessage.defaultResponse();
+        if(StringUtils.isNotBlank(title)){
+            return responseMessage;
+        }
+        if(id==null||id.equals("")){
+            List<PoiEntity> list= poiMapper.checkTitle("",title);
+            if(list==null||list.size()<=0){
+                return responseMessage;
+            }
+        }else{
+            PoiEntity pEntity=poiMapper.selectByPrimaryKey(id);
+            if(pEntity.getTitle().equals(title)&&pEntity.getId().equals(id)){
+                return responseMessage;
+            }
+        }
+        return responseMessage.setStatus(ResponseMessage.FAILED).setMsg("标题名称重复！");
+    }
+}
