790a393c912f1cdd5b68ebc396ff41462c585cff
e <1398975391@qq.com>
2019-10-11 14:32:53 +0800

    审核记录实体

---
+++ src/main/java/cn/com/wanwei/bic/mapper/AuditLogMapper.java
@@ -0,0 +1,26 @@
+package cn.com.wanwei.bic.mapper;
+
+import cn.com.wanwei.bic.entity.AuditLogEntity;
+import cn.com.wanwei.bic.entity.PoiEntity;
+import com.github.pagehelper.Page;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Map;
+
+@Mapper
+public interface AuditLogMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(AuditLogEntity record);
+
+    AuditLogEntity selectByPrimaryKey(String id);
+
+    int updateByPrimaryKey(AuditLogEntity record);
+
+    /**
+     * 审核记录管理分页列表
+     * @param filter
+     * @return
+     */
+    Page<PoiEntity> findByPage(Map<String, Object> filter);
+}
