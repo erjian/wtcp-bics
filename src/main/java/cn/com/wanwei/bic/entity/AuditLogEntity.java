c2015cd575f41163adfd163d84d73226adf8769a
e <1398975391@qq.com>
2019-10-16 09:33:41 +0800

    代码优化

--- src/main/java/cn/com/wanwei/bic/entity/AuditLogEntity.java
+++ src/main/java/cn/com/wanwei/bic/entity/AuditLogEntity.java
@@ -1,5 +1,6 @@
 package cn.com.wanwei.bic.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -27,7 +28,7 @@ public class AuditLogEntity implements Serializable {
     @ApiModelProperty(value="关联信息ID" ,required = true)
     private String principalId;
 
-    @ApiModelProperty(value="审核前状态（0：待审，1：通过，2：退回，9：上线）" ,required = true)
+    @ApiModelProperty(value="审核前状态（0：待审，1：通过，2：退回，9：上线）")
     private Integer preStatus;
 
     @ApiModelProperty(value="审核状态（0：待审，1：通过，2：退回，9：上线）" ,required = true)
@@ -40,6 +41,7 @@ public class AuditLogEntity implements Serializable {
     private String createdUser;
 
     @ApiModelProperty(value="创建时间" ,readOnly = true)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
     private Date createdDate;
 
     @ApiModelProperty(value="操作记录类型（0：审核记录，1：上线/下线记录）" ,readOnly = true)
