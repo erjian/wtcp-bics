bc051ab70ce35eb282d12bc98f112a2a03f16b1f
cuizy <850043648@qq.com>
2019-11-11 10:45:24 +0800

    增加保存接口

--- src/main/java/cn/com/wanwei/bic/controller/MaterialController.java
+++ src/main/java/cn/com/wanwei/bic/controller/MaterialController.java
@@ -1,5 +1,6 @@
 package cn.com.wanwei.bic.controller;
 
+import cn.com.wanwei.bic.entity.MaterialEntity;
 import cn.com.wanwei.bic.service.MaterialService;
 import cn.com.wanwei.common.log.annotation.OperationLog;
 import cn.com.wanwei.common.model.ResponseMessage;
@@ -12,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RefreshScope
@@ -88,6 +91,18 @@ public class MaterialController extends BaseController {
     @OperationLog(value = "wtcp-bic/根据关联ID和主键更新图片素材标识", operate = "u", module = "素材管理")
     public ResponseMessage updateIdentify(@RequestParam String principalId, @RequestParam String id, @RequestParam Integer identify) throws Exception {
         return ResponseMessage.defaultResponse().setData(materialService.updateIdentify(principalId, id, identify, getCurrentUser()));
+    }
+
+    @ApiOperation(value = "根据关联ID保存素材", notes = "根据关联ID保存素材")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "principalId", value = "关联ID", required = true),
+            @ApiImplicitParam(name = "materialList", value = "素材主键", required = true)
+    })
+    @RequestMapping(value = "/batchInsert", method = RequestMethod.PUT)
+    @PreAuthorize("hasAuthority('material:u')")
+    @OperationLog(value = "batchInsert/根据关联ID保存素材", operate = "批量保存素材", module = "素材管理")
+    public ResponseMessage batchInsert(@RequestParam String principalId, @RequestBody List<MaterialEntity> materialList) throws Exception {
+        return ResponseMessage.defaultResponse().setData(materialService.batchInsert(principalId, materialList, getCurrentUser()));
     }
 
 }
