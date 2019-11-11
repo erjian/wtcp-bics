d4824db43a27404bd18cfbb2a25debb785ac74a7
cuizy <850043648@qq.com>
2019-11-11 14:50:58 +0800

    增加排序策略

--- src/main/java/cn/com/wanwei/bic/utils/PageUtils.java
+++ src/main/java/cn/com/wanwei/bic/utils/PageUtils.java
@@ -3,9 +3,11 @@ package cn.com.wanwei.bic.utils;
 import cn.com.wanwei.persistence.mybatis.MybatisPageRequest;
 import com.github.pagehelper.PageHelper;
 import com.google.common.collect.Lists;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Sort;
 
 import java.util.List;
+import java.util.Map;
 
 public class PageUtils {
 
@@ -18,10 +20,18 @@ public class PageUtils {
         return instance;
     }
 
-    public MybatisPageRequest setPage(Integer page, Integer size, Sort.Direction sortBy, String... sortFields){
+    public MybatisPageRequest setPage(Integer page, Integer size, Map<String, Object> filter, Sort.Direction sortBy, String... sortFields){
         List<Sort.Order> sortList = Lists.newArrayList();
-        for(String field : sortFields){
-            sortList.add(new Sort.Order(sortBy, field));
+        if(filter.containsKey("sortField") && null != filter.get("sortField") && StringUtils.isNotEmpty(filter.get("sortField").toString())){
+            Sort.Direction orderBy = Sort.Direction.DESC;
+            if(filter.containsKey("orderBy") && StringUtils.equals("acs", filter.get("orderBy").toString())){
+                orderBy = Sort.Direction.ASC;
+            }
+            sortList.add(new Sort.Order(orderBy, filter.get("sortField").toString()));
+        }else{
+            for(String field : sortFields){
+                sortList.add(new Sort.Order(sortBy, field));
+            }
         }
         Sort sort = Sort.by(sortList);
         MybatisPageRequest pageRequest = MybatisPageRequest.of(page, size, sort);
