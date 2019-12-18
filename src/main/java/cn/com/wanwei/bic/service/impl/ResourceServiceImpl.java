7401d15c28ffcc9f76bebfe8d2329553bad05527
lijie <TdHM*mJ2>
2019-12-18 18:01:53 +0800

    修改资源编目接口

--- src/main/java/cn/com/wanwei/bic/service/impl/ResourceServiceImpl.java
+++ src/main/java/cn/com/wanwei/bic/service/impl/ResourceServiceImpl.java
@@ -1,6 +1,5 @@
 package cn.com.wanwei.bic.service.impl;
 
-import cn.com.wanwei.bic.entity.ScenicEntity;
 import cn.com.wanwei.bic.feign.AuthServiceFeign;
 import cn.com.wanwei.bic.mapper.*;
 import cn.com.wanwei.bic.service.ResourceService;
@@ -17,7 +16,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * @author  **
+ * @author **
  */
 @Service
 @Slf4j
@@ -50,63 +49,64 @@ public class ResourceServiceImpl implements ResourceService {
 
     @Override
     public ResponseMessage findByPieChart(User user) {
-        List<Map<String,Object>> list = new ArrayList<>();
-        Map<String,Object> map1 = new HashMap<>();
+        List<Map<String, Object>> list = new ArrayList<>();
+        Map<String, Object> map1 = new HashMap<>();
         Long scenicCount = scenicMapper.findByDeptCode(user.getOrg().getCode());
-        map1.put("value",scenicCount);
-        map1.put("name","景区");
+        map1.put("value", scenicCount);
+        map1.put("name", "景区");
         list.add(map1);
         Long peripheryCount = peripheryMapper.findByDeptCode(user.getOrg().getCode());
-        Map<String,Object> map2 = new HashMap<>();
-        map2.put("value",peripheryCount);
-        map2.put("name","周边");
+        Map<String, Object> map2 = new HashMap<>();
+        map2.put("value", peripheryCount);
+        map2.put("name", "周边");
         list.add(map2);
         Long travelCount = travelAgentMapper.findByDeptCode(user.getOrg().getCode());
-        Map<String,Object> map3 = new HashMap<>();
-        map3.put("value",travelCount);
-        map3.put("name","从业");
+        Map<String, Object> map3 = new HashMap<>();
+        map3.put("value", travelCount);
+        map3.put("name", "从业");
         list.add(map3);
         Long rentalCount = rentalCarMapper.findByDeptCode(user.getOrg().getCode());
         Long trafficCount = trafficAgentMapper.findByDeptCode(user.getOrg().getCode());
-        Map<String,Object> map4 = new HashMap<>();
-        map4.put("value",rentalCount + trafficCount);
-        map4.put("name","出行");
+        Map<String, Object> map4 = new HashMap<>();
+        map4.put("value", rentalCount + trafficCount);
+        map4.put("name", "出行");
         list.add(map4);
         Long entertainmentCount = entertainmentMapper.findByDeptCode(user.getOrg().getCode());
         Long driveCount = driveCampMapper.findByDeptCode(user.getOrg().getCode());
-        Map<String,Object> map5 = new HashMap<>();
-        map5.put("value",entertainmentCount + driveCount);
-        map5.put("name","休闲娱乐");
+        Map<String, Object> map5 = new HashMap<>();
+        map5.put("value", entertainmentCount + driveCount);
+        map5.put("name", "休闲娱乐");
         list.add(map5);
         return ResponseMessage.defaultResponse().setData(list);
     }
 
     @Override
     public ResponseMessage findByHistogram(User user) {
-        Map<String,Object> nameMap = new HashMap<>();
+        Map<String, Object> nameMap = new HashMap<>();
         List<String> nameList = new ArrayList<>();
         List<String> codeList = new ArrayList<>();
-        nameMap.put("deptName",nameList);
+        nameMap.put("deptName", nameList);
         //获取组织机构数据
         ResponseMessage responseMessage = authServiceFeign.findByDeptCode(user.getOrg().getCode());
-        List<Map<String,String>> list = (List<Map<String, String>>) responseMessage.getData();
-        for (Map<String,String> map : list){
+        List<Map<String, String>> list = (List<Map<String, String>>) responseMessage.getData();
+        for (Map<String, String> map : list) {
             nameList.add(map.get("name"));
             codeList.add(map.get("code"));
         }
-        Map<String,Object> histogramMap = this.histogramData(codeList,user);
-        histogramMap.put("deptName",nameList);
+        Map<String, Object> histogramMap = this.histogramData(codeList, user);
+        histogramMap.put("deptName", nameList);
         return ResponseMessage.defaultResponse().setData(histogramMap);
     }
 
     /**
      * 景区、周边、从业、出行、休闲娱乐的数量
+     *
      * @param codeList 组织机构编码
-     * @param user 用户
+     * @param user     用户
      * @return Map
      */
-    private Map<String,Object> histogramData(List<String> codeList,User user){
-        Map<String,Object> map = new HashMap<>();
+    private Map<String, Object> histogramData(List<String> codeList, User user) {
+        Map<String, Object> map = new HashMap<>();
         // 景区
         List<Long> scenicList = new ArrayList<>();
         // 周边
@@ -117,7 +117,7 @@ public class ResourceServiceImpl implements ResourceService {
         List<Long> travelList = new ArrayList<>();
         // 休闲娱乐
         List<Long> entertainmentList = new ArrayList<>();
-        for (String deptCode : codeList){
+        for (String deptCode : codeList) {
             Long scenicCount = scenicMapper.findByDeptCode(deptCode);
             scenicList.add(scenicCount);
             Long peripheryCount = peripheryMapper.findByDeptCode(user.getOrg().getCode());
@@ -131,11 +131,11 @@ public class ResourceServiceImpl implements ResourceService {
             Long driveCount = driveCampMapper.findByDeptCode(user.getOrg().getCode());
             entertainmentList.add(entertainmentCount + driveCount);
         }
-        map.put("景区",scenicList);
-        map.put("周边",peripheryList);
-        map.put("从业",employedList);
-        map.put("出行",travelList);
-        map.put("休闲",entertainmentList);
+        map.put("景区", scenicList);
+        map.put("周边", peripheryList);
+        map.put("从业", employedList);
+        map.put("出行", travelList);
+        map.put("休闲", entertainmentList);
         return map;
     }
 
