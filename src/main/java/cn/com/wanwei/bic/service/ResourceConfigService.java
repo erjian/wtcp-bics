1c1dca3d20a3f93f013dd8f19e3965a4e58e2072
linjianwu <linjw824@>
2019-12-20 11:49:02 +0800

    新增的资源配置的service层

---
+++ src/main/java/cn/com/wanwei/bic/service/ResourceConfigService.java
@@ -0,0 +1,58 @@
+/**
+ * 该源代码文件 ResourceConfigService.java 是工程“wtcp-bics”的一部分。
+ * @project wtcp-bics
+ * @author 蔺健武
+ * @date 2019年12月20日11:14:02
+ */
+package cn.com.wanwei.bic.service;
+
+import cn.com.wanwei.bic.entity.ResourceConfigEntity;
+import cn.com.wanwei.bic.entity.ScenicEntity;
+import cn.com.wanwei.bic.model.DataBindModel;
+import cn.com.wanwei.bic.model.EntityTagsModel;
+import cn.com.wanwei.bic.model.WeightModel;
+import cn.com.wanwei.common.model.ResponseMessage;
+import cn.com.wanwei.common.model.User;
+
+import java.util.Map;
+
+/**
+ * wtcp-bics - ResourceConfigService 资源配置信息管理接口
+ */
+public interface ResourceConfigService {
+
+	/**
+	 * 新增一条记录
+	 * @auth linjw 2019年12月20日11:14:30
+	 * @param entity
+	 * @param user
+	 * @return
+	 */
+	ResponseMessage save(ResourceConfigEntity entity, User user) throws Exception;
+
+	/**
+	 * 编辑一条记录
+	 * @auth linjw 2019年12月20日11:14:30
+	 * @param id
+	 * @param entity
+	 * @param user
+	 * @return
+	 */
+	ResponseMessage edit(String id, ResourceConfigEntity entity, User user) throws Exception;
+
+	/**
+	 * 根据id查询一条记录
+	 * @auth linjw 2019年12月20日11:15:03
+	 * @param id
+	 * @return
+	 */
+	ResponseMessage selectByPrimaryKey(String id) throws Exception;
+
+	/**
+	 * 根据code查询一条记录
+	 * @auth linjw 2019年12月20日11:18:06
+	 * @param code
+	 * @return
+	 */
+	ResponseMessage selectByCode(String code) throws Exception;
+}
---
+++ src/main/java/cn/com/wanwei/bic/service/impl/ResourceConfigServiceImpl.java
@@ -0,0 +1,70 @@
+/**
+ * 该源代码文件 ResourceConfigServiceImpl.java 是工程“wtcp-bics”的一部分。
+ *
+ * @project wtcp-bics
+ * @author 蔺健武
+ * @date 2019年12月20日11:20:05
+ */
+package cn.com.wanwei.bic.service.impl;
+
+import cn.com.wanwei.bic.entity.ResourceConfigEntity;
+import cn.com.wanwei.bic.mapper.ResourceConfigMapper;
+import cn.com.wanwei.bic.service.ResourceConfigService;
+import cn.com.wanwei.bic.service.ScenicService;
+import cn.com.wanwei.bic.utils.UUIDUtils;
+import cn.com.wanwei.common.model.ResponseMessage;
+import cn.com.wanwei.common.model.User;
+import com.github.pagehelper.util.StringUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+/**
+ * wtcp-bics - ResourceConfigServiceImpl 景资源配置信息管理接口实现类
+ */
+@Service
+@Slf4j
+@RefreshScope
+public class ResourceConfigServiceImpl implements ResourceConfigService {
+
+    @Autowired
+    private ResourceConfigMapper resourceConfigMapper;
+
+    @Override
+    public ResponseMessage save(ResourceConfigEntity entity, User user) throws Exception {
+        String id = UUIDUtils.getInstance().getId();
+        entity.setId(id);
+        entity.setCreatedUser(user.getUsername());
+        entity.setCreatedDate(new Date());
+        resourceConfigMapper.insert(entity);
+        return ResponseMessage.defaultResponse().setMsg("保存成功");
+    }
+
+    @Override
+    public ResponseMessage edit(String id, ResourceConfigEntity entity, User user) throws Exception {
+        ResourceConfigEntity resourceConfigEntity = resourceConfigMapper.selectByPrimaryKey(id);
+        if(null == resourceConfigEntity){
+            return ResponseMessage.validFailResponse().setMsg("该配置信息不存在");
+        }
+        entity.setId(id);
+        entity.setCreatedDate(resourceConfigEntity.getCreatedDate());
+        entity.setCreatedUser(resourceConfigEntity.getCreatedUser());
+        entity.setUpdatedDate(new Date());
+        entity.setUpdatedUser(user.getUsername());
+        resourceConfigMapper.updateByPrimaryKey(entity);
+        return ResponseMessage.defaultResponse().setMsg("更新成功");
+    }
+
+    @Override
+    public ResponseMessage selectByPrimaryKey(String id) throws Exception {
+        return ResponseMessage.defaultResponse().setData(resourceConfigMapper.selectByPrimaryKey(id));
+    }
+
+    @Override
+    public ResponseMessage selectByCode(String code) throws Exception {
+        return ResponseMessage.defaultResponse().setData(resourceConfigMapper.selectByCode(code));
+    }
+}
