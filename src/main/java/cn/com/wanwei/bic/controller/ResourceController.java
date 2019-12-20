33f7d53fda7c7d8843ae89434e7dd6fc02cadd20
linjianwu <linjw824@>
2019-12-20 18:15:23 +0800

    修改的接口

--- src/main/java/cn/com/wanwei/bic/controller/ResourceController.java
+++ src/main/java/cn/com/wanwei/bic/controller/ResourceController.java
@@ -3,13 +3,15 @@ package cn.com.wanwei.bic.controller;
 import cn.com.wanwei.bic.service.*;
 import cn.com.wanwei.common.model.ResponseMessage;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 /**
  * @author
@@ -26,15 +28,25 @@ public class ResourceController extends BaseController{
     private ResourceService resourceService;
 
     @ApiOperation(value = "饼状图", notes = "饼状图")
-    @RequestMapping(value = "/findByPieChart", method = RequestMethod.GET)
-    public ResponseMessage findByPieChart()throws Exception{
-        return resourceService.findByPieChart(getCurrentUser());
+    @RequestMapping(value = "/findByPieChart", method = RequestMethod.POST)
+    public ResponseMessage findByPieChart(@RequestBody Map<String, Object> queryModel)throws Exception{
+        return resourceService.findByPieChart(getCurrentUser(), queryModel);
     }
 
-    @ApiOperation(value = "柱状图", notes = "柱状图")
-    @RequestMapping(value = "/findByHistogram", method = RequestMethod.GET)
-    public ResponseMessage findByHistogram() throws Exception{
-        return resourceService.findByHistogram(getCurrentUser());
+    @ApiOperation(value = "根据资源编码Code获取饼状图的数据", notes = "根据资源编码Code获取饼状图的数据")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "code", value = "资源编码", required = true),
+            @ApiImplicitParam(name = "size", value = "资源子级资源数量", required = true)
+    })
+    @RequestMapping(value = "/initPieByCode", method = RequestMethod.GET)
+    public ResponseMessage initPieByCode(@RequestParam String code, @RequestParam Integer size) throws Exception{
+        return resourceService.initPieByCode(getCurrentUser(), code, size);
     }
+
+//    @ApiOperation(value = "柱状图", notes = "柱状图")
+//    @RequestMapping(value = "/findByHistogram", method = RequestMethod.GET)
+//    public ResponseMessage findByHistogram() throws Exception{
+//        return resourceService.findByHistogram(getCurrentUser());
+//    }
 
 }
