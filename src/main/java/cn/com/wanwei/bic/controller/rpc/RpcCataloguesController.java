2453db99e8df3ed87ce0ef1d6d2d1ce454f19684
linjianwu <linjw824@>
2020-02-07 10:44:00 +0800

    新增资源类型枚举和获取资源类型的接口；修改根据资源类型获取数据的接口

--- src/main/java/cn/com/wanwei/bic/controller/rpc/RpcCataloguesController.java
+++ src/main/java/cn/com/wanwei/bic/controller/rpc/RpcCataloguesController.java
@@ -1,7 +1,10 @@
 package cn.com.wanwei.bic.controller.rpc;
 
+import cn.com.wanwei.bic.model.DataType;
 import cn.com.wanwei.bic.service.*;
 import cn.com.wanwei.common.model.ResponseMessage;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -13,6 +16,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * wtcp-bics/catalogues
@@ -48,16 +54,6 @@ public class RpcCataloguesController {
     @Autowired
     private TrafficAgentService trafficAgentService;
 
-    private static final String SCENIC_TYPE = "125001001";        //普通景区
-    private static final String TOUR_VILLAGE_TYPE = "125001002";  //旅游示范村
-    private static final String TRAVEL_TYPE = "125003001";        // 旅行社
-    private static final String AGRITAINMENT_TYPE = "125005002";  // 农家乐
-    private static final String FOOD_TYPE = "125006002";          //  餐饮
-    private static final String DRIVE_CAMP_TYPE = "125005001";    // 自驾营地
-    private static final String SHOPPING_TYPE = "125006004";      // 购物
-    private static final String RENTAL_CAR_TYPE = "125004001";    // 租车
-    private static final String TRAFFIC_AGENT_TYPE = "125004002"; // 交通枢纽
-
     @ApiOperation(value = "根据资源编码获取资源", notes = "根据资源编码获取资源")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "type", value = "资源编码", required = true, dataType = "String"),
@@ -67,34 +63,29 @@ public class RpcCataloguesController {
     @GetMapping("/getDataByType")
     public ResponseMessage getDataByType(@RequestParam String type, String name, String ids) throws Exception {
         ResponseMessage responseMessage;
-        switch (type) {
-            case SCENIC_TYPE:
-            case TOUR_VILLAGE_TYPE:
-                responseMessage = scenicService.findBySearchValue(type,name,ids); //旅游示范村
-                break;
-            case TRAVEL_TYPE:
-                responseMessage =  travelAgentService.findBySearchValue(name,ids);  // 旅行社
-                break;
-            case FOOD_TYPE:
-            case SHOPPING_TYPE:
-                responseMessage =  peripheryService.findBySearchValue(type, name,ids);  // 购物
-                break;
-            case AGRITAINMENT_TYPE:
-                responseMessage =  entertainmentService.findBySearchValue(type, name,ids);  // 农家乐
-                break;
-            case RENTAL_CAR_TYPE:
-                responseMessage =  rentalCarService.findBySearchValue(name,ids);  // 租车
-                break;
-            case TRAFFIC_AGENT_TYPE:
-                responseMessage =  trafficAgentService.findBySearchValue(name,ids);  // 交通枢纽
-                break;
-            case DRIVE_CAMP_TYPE:
-                responseMessage =  driveCampService.findBySearchValue(name,ids);  // 自驾营地
-                break;
-            default:
-                responseMessage =  ResponseMessage.validFailResponse().setMsg("获取失败");
-                break;
+        if(type.equals(DataType.SCENIC_TYPE.getKey()) || type.equals(DataType.TOUR_VILLAGE_TYPE.getKey())){
+            responseMessage = scenicService.findBySearchValue(type,name,ids);
+        }else if(type.equals(DataType.TRAVEL_TYPE.getKey())){
+            responseMessage =  travelAgentService.findBySearchValue(name,ids);
+        }else if(type.equals(DataType.FOOD_TYPE.getKey()) || type.equals(DataType.SHOPPING_TYPE.getKey())){
+            responseMessage =  peripheryService.findBySearchValue(type, name,ids);
+        }else if(type.equals(DataType.AGRITAINMENT_TYPE.getKey())){
+            responseMessage =  entertainmentService.findBySearchValue(type, name,ids);
+        }else if(type.equals(DataType.RENTAL_CAR_TYPE.getKey())){
+            responseMessage =  rentalCarService.findBySearchValue(name,ids);
+        }else if(type.equals(DataType.TRAFFIC_AGENT_TYPE.getKey())){
+            responseMessage =  trafficAgentService.findBySearchValue(name,ids);
+        }else if(type.equals(DataType.DRIVE_CAMP_TYPE.getKey())){
+            responseMessage =  driveCampService.findBySearchValue(name,ids);
+        }else {
+            responseMessage =  ResponseMessage.validFailResponse().setMsg("获取失败");
         }
         return responseMessage;
+    }
+
+    @ApiOperation(value = "获取资源类型", notes = "获取资源类型")
+    @GetMapping("/getDataType")
+    public ResponseMessage getDataType(){
+        return ResponseMessage.defaultResponse().setData(DataType.list());
     }
 }
---
+++ src/main/java/cn/com/wanwei/bic/model/DataType.java
@@ -0,0 +1,66 @@
+/**
+ * 该源代码文件 DataType 是工程“wtcp-bics”的一部分。
+ *
+ * @project wtcp-bics
+ * @author 蔺健武
+ * @date 2020年2月7日10:06:30
+ */
+package cn.com.wanwei.bic.model;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * wtcp-bics - DataType 资源类型
+ */
+public enum DataType {
+    SCENIC_TYPE("125001001","普通景区"),
+    TOUR_VILLAGE_TYPE("125001002","旅游示范村"),
+    TRAVEL_TYPE("125003001","旅行社"),
+    RENTAL_CAR_TYPE("125004001","租车"),
+    TRAFFIC_AGENT_TYPE("125004002","交通枢纽"),
+    DRIVE_CAMP_TYPE("125005001","自驾营地"),
+    AGRITAINMENT_TYPE("125005002","农家乐"),
+    FOOD_TYPE("125006002","餐饮"),
+    SHOPPING_TYPE("125006004","购物");
+
+    private String key;
+    private String name;
+
+    DataType(String key, String name) {
+        this.key = key;
+        this.name = name;
+    }
+
+    public static List<Map<String, Object>> list() {
+        List<Map<String, Object>> list = Lists.newArrayList();
+        for (DataType item : DataType.values()) {
+            Map<String, Object> itemMap = Maps.newHashMap();
+            itemMap.put("key", item.getKey());
+            itemMap.put("name", item.getName());
+            list.add(itemMap);
+        }
+        return list;
+    }
+
+    public static String getName(String key) {
+        for(DataType item : DataType.values()) {
+            if(item.getKey().equals(key)) {
+                return item.name;
+            }
+        }
+        return null;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
