af286cb0b52fea87d59934d834a758e2230e1600
linjianwu <linjw824@>
2020-02-12 16:03:33 +0800

    新增的同步数据的接口

--- src/main/java/cn/com/wanwei/bic/controller/rpc/RpcCataloguesController.java
+++ src/main/java/cn/com/wanwei/bic/controller/rpc/RpcCataloguesController.java
@@ -3,22 +3,18 @@ package cn.com.wanwei.bic.controller.rpc;
 import cn.com.wanwei.bic.model.DataType;
 import cn.com.wanwei.bic.service.*;
 import cn.com.wanwei.common.model.ResponseMessage;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Map;
 
 /**
  * wtcp-bics/catalogues
@@ -53,6 +49,29 @@ public class RpcCataloguesController {
 
     @Autowired
     private TrafficAgentService trafficAgentService;
+
+    @Value("${wtcp.bic.syncSize}")
+    protected Integer syncSize;
+
+    @ApiOperation(value = "定时同步数据server端接口", notes = "定时同步数据server端接口")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page", value = "页号", defaultValue = "0"),
+            @ApiImplicitParam(name = "size", value = "每页数量,最大值为500", defaultValue = "500"),
+            @ApiImplicitParam(name = "syncType", value = "同步类型：0 全量 1 增量，默认为增量"),
+            @ApiImplicitParam(name = "syncDate", value = "同步日期，格式：yyyy-MM-dd"),
+            @ApiImplicitParam(name = "category", value = "资源类型", required = true, dataType = "String")
+    })
+    @GetMapping("/SyncDataServer")
+    public ResponseMessage SyncDataServer(@RequestParam(value = "page", defaultValue = "0") Integer page,
+                                          @RequestParam(value = "size", defaultValue = "500") Integer size,
+                                          @RequestParam(value = "syncType", defaultValue = "1") Integer syncType,
+                                          @RequestParam(value = "syncDate") String syncDate,
+                                          @RequestParam(value = "category") String category){
+        if(size>syncSize){
+            return ResponseMessage.validFailResponse().setMsg("每页数据条数size的值超过了最大限定值"+syncSize);
+        }
+        return ResponseMessage.defaultResponse();
+    }
 
     @ApiOperation(value = "根据资源编码获取资源", notes = "根据资源编码获取资源")
     @ApiImplicitParams({
