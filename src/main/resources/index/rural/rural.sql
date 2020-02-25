8bf970d3bc177affc6c2ea65ed0bad2d55a316b9
cuizy <850043648@qq.com>
2020-02-25 17:03:14 +0800

    增加索引字段

--- src/main/resources/index/rural/rural.sql
+++ src/main/resources/index/rural/rural.sql
@@ -3,6 +3,7 @@
  */
 
 select t.id relateId,t.id,t.`code`, t.title,t.sub_title subTitle,t.slogan,t.summary, t.description,
+t.start_time startTime,t.summer_time summerTime, t.winter_time winterTime,
 t.address,t.scenic_id scenicId, t.within_scenic withinScenic, t.within_park withinPark,t.num,t.weight,t.type,
 t.latitude, t.longitude,CONCAT(t.latitude,",",t.longitude) geoPoint,t.region,t.region_full_code regionFullCode,t.region_full_name regionFullName,
 t.dept_code deptCode,'' images,'' videos,'' audios, '' tags,'' relateTags, '' allTags, if(t.status=9, 1,0) publishStatus,
