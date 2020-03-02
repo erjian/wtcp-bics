4010570b351a3217a9849c4b26cac722e704632c
linjianwu <linjw824@>
2020-03-02 17:45:28 +0800

    feat(场馆管理)：新增dao层接口

--- src/main/java/cn/com/wanwei/bic/mapper/VenueMapper.java
+++ src/main/java/cn/com/wanwei/bic/mapper/VenueMapper.java
@@ -1,7 +1,14 @@
 package cn.com.wanwei.bic.mapper;
 
 import cn.com.wanwei.bic.entity.VenueEntity;
+import cn.com.wanwei.common.annotation.DataScope;
+import com.github.pagehelper.Page;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface VenueMapper {
@@ -12,4 +19,29 @@ public interface VenueMapper {
     VenueEntity selectByPrimaryKey(String id);
 
     int updateByPrimaryKey(VenueEntity record);
+
+    /**
+     * 获取分页列表
+     * @param filter 查询参数
+     * @return Page<VenueEntity>
+     */
+    @DataScope
+    Page<VenueEntity> findByPage(Map<String, Object> filter);
+
+    Page<VenueEntity> findByPageForFeign(Map<String, Object> filter);
+
+    /**
+     * 关联机构
+     * @param updatedUser
+     * @param updatedDate
+     * @param deptCode
+     * @param ids
+     * @return
+     */
+    int dataBind(@Param(value="updatedUser") String updatedUser, @Param(value="updatedDate") Date updatedDate,
+                 @Param(value="deptCode") String deptCode, @Param(value="ids") List<String> ids);
+
+    List<VenueEntity> getVenueInfo(@Param(value="title") String title, @Param(value="status") Integer status);
+
+    List<VenueEntity> findByTitleAndIdNot(@Param(value="title")String title, @Param(value="id")String id);
 }
