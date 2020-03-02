82b4077349930c2b85295d1b11ad3b3dfe26f4c9
linjianwu <linjw824@>
2020-03-02 11:54:09 +0800

    feat(场馆管理)：新增场馆信息dao层mapper文件和接口

---
+++ src/main/java/cn/com/wanwei/bic/mapper/VenueMapper.java
@@ -0,0 +1,17 @@
+package cn.com.wanwei.bic.mapper;
+
+import cn.com.wanwei.bic.entity.VenueEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface VenueMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(VenueEntity record);
+
+    VenueEntity selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeyWithBLOBs(VenueEntity record);
+
+    int updateByPrimaryKey(VenueEntity record);
+}
