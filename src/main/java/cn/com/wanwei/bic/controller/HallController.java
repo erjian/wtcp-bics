e36a8fc73b97074a91c89f537df33a5ef2e39b4d
cuizy <850043648@qq.com>
2020-03-05 09:29:44 +0800

    refactor(场馆厅室):修改厅室相关接口为公共接口

--- src/main/java/cn/com/wanwei/bic/controller/HallController.java
+++ src/main/java/cn/com/wanwei/bic/controller/HallController.java
@@ -1,14 +1,17 @@
 package cn.com.wanwei.bic.controller;
 
 import cn.com.wanwei.bic.entity.HallEntity;
-import cn.com.wanwei.bic.model.DataBindModel;
 import cn.com.wanwei.bic.model.EntityTagsModel;
 import cn.com.wanwei.bic.model.WeightModel;
 import cn.com.wanwei.bic.service.CommonService;
 import cn.com.wanwei.bic.service.HallService;
 import cn.com.wanwei.common.log.annotation.OperationLog;
 import cn.com.wanwei.common.model.ResponseMessage;
+import cn.com.wanwei.common.model.User;
 import cn.com.wanwei.common.utils.RequestUtil;
+import cn.com.wanwei.mybatis.model.DeptCodeBindModel;
+import cn.com.wanwei.persistence.mybatis.utils.EscapeCharUtils;
+import com.google.common.base.Strings;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -50,7 +53,8 @@ public class HallController extends BaseController {
                                       @RequestParam(value = "size", defaultValue = "10") Integer size,
                                       HttpServletRequest request) {
         Map<String, Object> filter = RequestUtil.getParameters(request);
-        return hallService.findByPage(page, size, filter);
+        EscapeCharUtils.escape(filter, "title");
+        return ResponseMessage.defaultResponse().setData(hallService.findByPageAndDeptCode(page, size, filter));
     }
 
     @ApiOperation(value = "获取场馆厅室列表（可根据所属场馆ID：venueId 和厅室名称：title 进行查询）",
@@ -62,9 +66,12 @@ public class HallController extends BaseController {
     @GetMapping(value = "/list")
     @PreAuthorize("hasAuthority('hall:r')")
     @OperationLog(value = "wtcp-bics/获取场馆厅室列表", operate = "r", module = "场馆厅室")
-    public ResponseMessage findByList(HttpServletRequest request) {
+    public ResponseMessage findByList(String title, String venueId, HttpServletRequest request) {
         Map<String, Object> filter = RequestUtil.getParameters(request);
-        return hallService.findByList(filter);
+        filter.put("title", Strings.isNullOrEmpty(title) ? "" : title);
+        filter.put("venueId", Strings.isNullOrEmpty(venueId) ? "" : venueId);
+        EscapeCharUtils.escape(filter, "title");
+        return ResponseMessage.defaultResponse().setData(hallService.findByList(filter));
     }
 
     @ApiOperation(value = "查询场馆厅室详情", notes = "根据ID查询场馆厅室详情")
@@ -72,12 +79,8 @@ public class HallController extends BaseController {
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @PreAuthorize("hasAuthority('hall:v')")
     @OperationLog(value = "wtcp-bics/根据id查询场馆厅室详情", operate = "v", module = "场馆厅室")
-    public ResponseMessage detail(@PathVariable("id") String id) {
-        HallEntity hallEntity = hallService.selectByPrimaryKey(id);
-        if (hallEntity == null) {
-            return ResponseMessage.validFailResponse().setMsg("数据不存在");
-        }
-        return ResponseMessage.defaultResponse().setData(hallEntity);
+    public ResponseMessage detail(@PathVariable("id") String id) throws Exception {
+        return ResponseMessage.defaultResponse().setData(hallService.findById(id));
     }
 
     @ApiOperation(value = "删除场馆厅室", notes = "根据ID删除场馆厅室")
@@ -86,7 +89,7 @@ public class HallController extends BaseController {
     @PreAuthorize("hasAuthority('hall:d')")
     @OperationLog(value = "wtcp-bics/根据id删除场馆厅室", operate = "d", module = "场馆厅室")
     public ResponseMessage delete(@PathVariable("id") String id) {
-        return hallService.deleteByPrimaryKey(id);
+        return hallService.deleteById(id);
     }
 
     @ApiOperation(value = "场馆厅室新增", notes = "场馆厅室新增")
@@ -98,7 +101,7 @@ public class HallController extends BaseController {
         if (bindingResult.hasErrors()) {
             return ResponseMessage.validFailResponse().setMsg(bindingResult.getAllErrors());
         }
-        return hallService.insert(hallModel, getCurrentUser(), ruleId, appCode);
+        return hallService.save(hallModel, getCurrentUser(), ruleId, appCode);
     }
 
     @ApiOperation(value = "场馆厅室编辑", notes = "场馆厅室编辑")
@@ -132,20 +135,22 @@ public class HallController extends BaseController {
             @ApiImplicitParam(name = "id", value = "场馆厅室ID", required = true, dataType = "String"),
             @ApiImplicitParam(name = "status", value = "上下线状态", required = true, dataType = "String")
     })
-    @RequestMapping(value = "/changeStatus", method = RequestMethod.GET)
-    public ResponseMessage changeStatus(@RequestParam String id, @RequestParam Integer status) throws Exception {
-        return hallService.changeStatus(id, status, getCurrentUser().getUsername());
+    @RequestMapping(value = "/updateOnlineStatus", method = RequestMethod.GET)
+    public ResponseMessage updateOnlineStatus(@RequestParam String id, @RequestParam Integer status) throws Exception {
+        User user = getCurrentUser();
+        return hallService.updateOnlineStatus(id, status, user.getUsername());
     }
 
     @PreAuthorize("hasAuthority('hall:b')")
     @ApiOperation(value = "组织机构切换", notes = "组织机构切换")
-    @ApiImplicitParams({@ApiImplicitParam(name = "model", value = "组织机构切换model", required = true, dataType = "DataBindModel")})
+    @ApiImplicitParams({@ApiImplicitParam(name = "deptCodeBindModel", value = "组织机构切换model", required = true, dataType = "DeptCodeBindModel")})
     @RequestMapping(value = "/dataBind", method = {RequestMethod.PUT, RequestMethod.PATCH})
-    public ResponseMessage dataBind(@RequestBody @Valid DataBindModel model, BindingResult bindingResult) throws Exception {
+    public ResponseMessage updateDeptCode(@RequestBody @Valid DeptCodeBindModel deptCodeBindModel, BindingResult bindingResult) throws Exception {
         if (bindingResult.hasErrors()) {
             return ResponseMessage.validFailResponse().setMsg(bindingResult.getAllErrors());
         }
-        return hallService.dataBind(getCurrentUser().getUsername(), model);
+        deptCodeBindModel.setCurrentUserName(getCurrentUser().getUsername());
+        return hallService.updateDeptCode(deptCodeBindModel);
     }
 
     @ApiOperation(value = "场馆厅室名称是否重复", notes = "场馆厅室名称是否重复(status:1 表示不重复，0表示重复)")
@@ -156,7 +161,7 @@ public class HallController extends BaseController {
     @RequestMapping(value = "/existByTitle", method = RequestMethod.GET)
     @OperationLog(value = "wtcp-bics/检查场馆厅室名称是否重复", operate = "", module = "场馆厅室", frontCode = "", resource = "")
     public ResponseMessage existByTitle(@RequestParam String title, String id) {
-        return hallService.existsByTitleAndIdNot(title, id != null ? id : "-1");
+        return hallService.existsByValueAndIdNot(title, id, "该名称已存在");
     }
 
 }
