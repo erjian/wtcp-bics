ad137ab38380ec12c0f020349edfce64296e817d
linjianwu <linjw824@>
2020-03-11 08:53:23 +0800

    feat(场馆厅室管理): 新增根据场馆id更新机构编码的接口

--- src/main/java/cn/com/wanwei/bic/mapper/HallMapper.java
+++ src/main/java/cn/com/wanwei/bic/mapper/HallMapper.java
@@ -5,9 +5,22 @@ import cn.com.wanwei.mybatis.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.Date;
+import java.util.List;
+
 @Mapper
 public interface HallMapper extends BaseMapper<HallEntity, String> {
 
     long countByVenueId(@Param("venueId") String venueId);
-    
+
+    /**
+     * 根据场馆id修改机构编码
+     * @auth linjw 2020年3月11日08:44:20
+     * @param updatedUser
+     * @param updatedDate
+     * @param deptCode
+     * @param ids
+     * @return
+     */
+    int updateDeptCodeByVenueId(@Param("updatedUser") String updatedUser, @Param("updatedDate") Date updatedDate, @Param("deptCode") String deptCode, @Param("ids") List<String> ids);
 }
