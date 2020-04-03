4124071f76d9bcba4cd3af1987a331bcdcc68e97
linjianwu <linjw824@>
2020-04-03 16:53:13 +0800

    fix(文旅资源管理中心): 租车添加查询并同步vr封面图、视频封面图的sql

--- src/main/resources/index/car/car.sql
+++ src/main/resources/index/car/car.sql
@@ -5,7 +5,7 @@
 select t.id relateId,t.id,t.`code`, t.title,t.sub_title subTitle,t.title_qp fullSpell, t.title_jp simpleSpell,t.slogan,
 t.summary, t.address,t.weight,t.establish_time,t.chartered_bus_type,t.regular_bus_type,t.keyword,t.latitude, t.longitude,
 CONCAT(t.latitude,",",t.longitude) geoPoint,t.region,t.region_full_code regionFullCode,t.region_full_name regionFullName,
-t.dept_code deptCode,'' images,'' videos,'' audios, '' tags,'' relateTags, '' allTags, if(t.status=9, 1,0) publishStatus,
+t.dept_code deptCode,'' vrCoverImage,'' videoCoverImage,'' images,'' videos,'' audios, '' tags,'' relateTags, '' allTags, if(t.status=9, 1,0) publishStatus,
 (select b.phone from t_bic_business b where b.principal_id = t.id) as phone
 from t_bic_rental_car t ;
 
@@ -27,4 +27,16 @@ GROUP BY t.principal_id;
 select t.principal_id relateId, CONCAT('[',GROUP_CONCAT('"',REPLACE(t.file_url,'\\','/'),'"'),']') audios
 from t_bic_material t
 where t.file_type = 'audio' and t.principal_id is not null
+GROUP BY t.principal_id;
+
+SELECT t.principal_id relateId,	CONCAT('[', GROUP_CONCAT(CONCAT('{"fileUrl":"', t.file_url, '","fileIdentify":"',IFNULL( t.file_identify, '' ),
+				'","coverImageUrl":"', IFNULL( t.cover_image_url, '' ),	'","fileName":"', IFNULL( t.file_name, '' ), '"}')), ']') vrCoverImage
+FROM t_bic_material t
+WHERE t.file_type = 'image'	AND t.principal_id IS NOT NULL AND t.principal_id != ''
+GROUP BY t.principal_id;
+
+SELECT t.principal_id relateId,	CONCAT('[',	GROUP_CONCAT(CONCAT('{"fileUrl":"',	t.file_url,	'","fileIdentify":"', IFNULL( t.file_identify, '' ), '","coverImageUrl":"',
+				IFNULL( t.cover_image_url, '' ), '","fileName":"', IFNULL( t.file_name, '' ), '"}')), ']') videoCoverImage
+FROM t_bic_material t
+WHERE t.file_type = 'video'	AND t.principal_id IS NOT NULL AND t.principal_id != ''
 GROUP BY t.principal_id;
