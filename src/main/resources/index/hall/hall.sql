b597505de13f4e6599a6896e1107ddfa7646d876
linjianwu <linjw824@>
2020-04-03 17:07:23 +0800

    fix(文旅资源管理中心): 场馆厅室添加查询并同步vr封面图、视频封面图的sql

--- src/main/resources/index/hall/hall.sql
+++ src/main/resources/index/hall/hall.sql
@@ -5,7 +5,7 @@
 select t.id relateId,t.id,t.`code`, t.title,t.full_spell fullSpell, t.simple_spell simpleSpell,
 t.summary, t.description,t.category,t.area,t.weight,t.vr_url vrUrl,t.max_reception_num maxReceptionNum,
 t.latitude, t.longitude,CONCAT(t.latitude,",",t.longitude) geoPoint,t.venue_id venueId,
-t.dept_code deptCode,'' images,'' videos,'' audios, '' tags,'' relateTags, '' allTags, if(t.status=9, 1,0) publishStatus
+t.dept_code deptCode,'' vrCoverImage,'' videoCoverImage,'' images,'' videos,'' audios, '' tags,'' relateTags, '' allTags, if(t.status=9, 1,0) publishStatus
 from t_bic_hall t;
 
 
@@ -22,4 +22,16 @@ GROUP BY t.principal_id;
 select t.principal_id relateId, CONCAT('[',GROUP_CONCAT('"',REPLACE(t.file_url,'\\','/'),'"'),']') audios
 from t_bic_material t
 where t.file_type = 'audio' and t.principal_id is not null
+GROUP BY t.principal_id;
+
+SELECT t.principal_id relateId,	CONCAT('[', GROUP_CONCAT(CONCAT('{"fileUrl":"', t.file_url, '","fileIdentify":"',IFNULL( t.file_identify, '' ),
+				'","coverImageUrl":"', IFNULL( t.cover_image_url, '' ),	'","fileName":"', IFNULL( t.file_name, '' ), '"}')), ']') vrCoverImage
+FROM t_bic_material t
+WHERE t.file_type = 'image'	AND t.principal_id IS NOT NULL AND t.principal_id != ''
+GROUP BY t.principal_id;
+
+SELECT t.principal_id relateId,	CONCAT('[',	GROUP_CONCAT(CONCAT('{"fileUrl":"',	t.file_url,	'","fileIdentify":"', IFNULL( t.file_identify, '' ), '","coverImageUrl":"',
+				IFNULL( t.cover_image_url, '' ), '","fileName":"', IFNULL( t.file_name, '' ), '"}')), ']') videoCoverImage
+FROM t_bic_material t
+WHERE t.file_type = 'video'	AND t.principal_id IS NOT NULL AND t.principal_id != ''
 GROUP BY t.principal_id;
