8c4a80dec684670da73203625b5b6684cc278e06
linjianwu <linjw824@>
2020-04-03 17:18:06 +0800

    fix(文旅资源管理中心): 旅行社添加查询并同步vr封面图、视频封面图的sql

--- src/main/resources/index/travel-agent/travel-agent.sql
+++ src/main/resources/index/travel-agent/travel-agent.sql
@@ -6,7 +6,7 @@ select t.id relateId,t.id,t.`code`, t.title,t.sub_title subTitle,t.full_spell fu
 t.summary, t.description, t.address,t.`level`,t.level_time levelTime,t.keyword,t.start_time startTime,t.from_city fromCity,
 t.destination_city destinationCity,t.business_type businessType,t.business_scope businessScope,t.tour_type tourType,t.team_type teamType,
 t.latitude, t.longitude,CONCAT(t.latitude,",",t.longitude) geoPoint,t.region,t.region_full_code regionFullCode,t.region_full_name regionFullName,
-t.dept_code deptCode,'' images,'' videos,'' audios, '' tags,'' relateTags, '' allTags, if(t.status=9, 1,0) publishStatus,
+t.dept_code deptCode,'' vrCoverImage,'' videoCoverImage,'' images,'' videos,'' audios, '' tags,'' relateTags, '' allTags, if(t.status=9, 1,0) publishStatus,
 (select b.phone from t_bic_business b where b.principal_id = t.id) as phone
 from t_bic_travel_agent t ;
 
@@ -28,4 +28,16 @@ GROUP BY t.principal_id;
 select t.principal_id relateId, CONCAT('[',GROUP_CONCAT('"',REPLACE(t.file_url,'\\','/'),'"'),']') audios
 from t_bic_material t
 where t.file_type = 'audio' and t.principal_id is not null
+GROUP BY t.principal_id;
+
+SELECT t.principal_id relateId,	CONCAT('[', GROUP_CONCAT(CONCAT('{"fileUrl":"', t.file_url, '","fileIdentify":"',IFNULL( t.file_identify, '' ),
+				'","coverImageUrl":"', IFNULL( t.cover_image_url, '' ),	'","fileName":"', IFNULL( t.file_name, '' ), '"}')), ']') vrCoverImage
+FROM t_bic_material t
+WHERE t.file_type = 'image'	AND t.principal_id IS NOT NULL AND t.principal_id != ''
+GROUP BY t.principal_id;
+
+SELECT t.principal_id relateId,	CONCAT('[',	GROUP_CONCAT(CONCAT('{"fileUrl":"',	t.file_url,	'","fileIdentify":"', IFNULL( t.file_identify, '' ), '","coverImageUrl":"',
+				IFNULL( t.cover_image_url, '' ), '","fileName":"', IFNULL( t.file_name, '' ), '"}')), ']') videoCoverImage
+FROM t_bic_material t
+WHERE t.file_type = 'video'	AND t.principal_id IS NOT NULL AND t.principal_id != ''
 GROUP BY t.principal_id;
