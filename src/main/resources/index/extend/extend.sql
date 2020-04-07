b1767541a88a81dcd529d1db826ac75a40e73729
linjianwu <linjw824@>
2020-04-07 14:31:10 +0800

    fix(文旅资源管理中心): ES索引添加vr封面图地址和视频封面图地址字段

--- src/main/resources/index/extend/extend.sql
+++ src/main/resources/index/extend/extend.sql
@@ -3,7 +3,8 @@
  */
 
 select t.id relateId,t.id,t.`code`,t.principal_id principalId,t.title,t.sub_title subTitle,
-t.weight,t.slogan,t.summary,t.description,t.content,t.type,t.price,t.dept_code deptCode,'' vrCoverImage,'' videoCoverImage,
+t.weight,t.slogan,t.summary,t.description,t.content,t.type,t.price,t.dept_code deptCode,
+(SELECT REPLACE(s.file_url,'\\','/') vrImage FROM t_bic_material s WHERE TRIM(s.file_identify)='vr' AND s.principal_id = t.id) vrImageUrl,	'' videoResources,
 '' images,'' videos,'' audios, '' tags,'' relateTags, '' allTags, if(t.status=9, 1,0) publishStatus
 from t_bic_extend t ;
 
@@ -27,14 +28,8 @@ from t_bic_material t
 where t.file_type = 'audio' and t.principal_id is not null
 GROUP BY t.principal_id;
 
-SELECT t.principal_id relateId,	CONCAT('[', GROUP_CONCAT(CONCAT('{"fileUrl":"', t.file_url, '","fileIdentify":"',IFNULL( t.file_identify, '' ),
-				'","coverImageUrl":"', IFNULL( t.cover_image_url, '' ),	'","fileName":"', IFNULL( t.file_name, '' ), '"}')), ']') vrCoverImage
-FROM t_bic_material t
-WHERE t.file_type = 'image'	AND t.principal_id IS NOT NULL AND t.principal_id != ''
-GROUP BY t.principal_id;
-
 SELECT t.principal_id relateId,	CONCAT('[',	GROUP_CONCAT(CONCAT('{"fileUrl":"',	t.file_url,	'","fileIdentify":"', IFNULL( t.file_identify, '' ), '","coverImageUrl":"',
-				IFNULL( t.cover_image_url, '' ), '","fileName":"', IFNULL( t.file_name, '' ), '"}')), ']') videoCoverImage
+				IFNULL( t.cover_image_url, '' ), '","fileName":"', IFNULL( t.file_name, '' ), '"}')), ']') videoResources
 FROM t_bic_material t
 WHERE t.file_type = 'video'	AND t.principal_id IS NOT NULL AND t.principal_id != ''
 GROUP BY t.principal_id;
