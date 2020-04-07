e892014c08b9eeceed52efe09fcc4d555feb00a8
linjianwu <linjw824@>
2020-04-07 16:35:13 +0800

    fix(文旅资源管理中心): ES索引添加vr封面图地址和视频封面图地址字段

--- src/main/resources/index/drive/drive.sql
+++ src/main/resources/index/drive/drive.sql
@@ -6,7 +6,7 @@ select t.id relateId,t.id,t.`code`, t.title,t.sub_title subTitle,t.full_spell fu
 t.slogan,t.start_time startTime,t.summer_time summerTime, t.winter_time winterTime,
 t.summary, t.description, t.address,t.start_time startTime,t.traffic,CONCAT(t.latitude,",",t.longitude) geoPoint,
 t.latitude, t.longitude,t.region,t.region_full_code regionFullCode,t.region_full_name regionFullName,t.weight,
-t.dept_code deptCode,(SELECT REPLACE(s.file_url,'\\','/') vrImage FROM t_bic_material s WHERE TRIM(s.file_identify)='vr' AND s.principal_id = t.id) vrImageUrl,	'' videoResources,'' images,'' videos,'' audios, '' tags,'' relateTags, '' allTags, if(t.status=9, 1,0) publishStatus,
+t.dept_code deptCode,(SELECT REPLACE(s.file_url,'\\','/') vrImage FROM t_bic_material s WHERE TRIM(s.file_identify)='vr' AND s.principal_id = t.id) vrImageUrl,	null videoResources,'' images,'' videos,'' audios, '' tags,'' relateTags, '' allTags, if(t.status=9, 1,0) publishStatus,
 (select b.phone from t_bic_business b where b.principal_id = t.id) as phone
 from t_bic_drive_camp t ;
 
