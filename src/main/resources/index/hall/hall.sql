a3410d52e358507fa426ed615c86d55c5b7f40cd
linjianwu <linjw824@>
2020-04-07 14:33:55 +0800

    fix(文旅资源管理中心): ES索引添加vr封面图地址和视频封面图地址字段

--- src/main/resources/index/hall/hall.sql
+++ src/main/resources/index/hall/hall.sql
@@ -5,7 +5,7 @@
 select t.id relateId,t.id,t.`code`, t.title,t.full_spell fullSpell, t.simple_spell simpleSpell,
 t.summary, t.description,t.category,t.area,t.weight,t.vr_url vrUrl,t.max_reception_num maxReceptionNum,
 t.latitude, t.longitude,CONCAT(t.latitude,",",t.longitude) geoPoint,t.venue_id venueId,
-t.dept_code deptCode,'' vrCoverImage,'' videoCoverImage,'' images,'' videos,'' audios, '' tags,'' relateTags, '' allTags, if(t.status=9, 1,0) publishStatus
+t.dept_code deptCode,(SELECT REPLACE(s.file_url,'\\','/') vrImage FROM t_bic_material s WHERE TRIM(s.file_identify)='vr' AND s.principal_id = t.id) vrImageUrl,	'' videoResources,'' images,'' videos,'' audios, '' tags,'' relateTags, '' allTags, if(t.status=9, 1,0) publishStatus
 from t_bic_hall t;
 
 
@@ -24,14 +24,8 @@ from t_bic_material t
 where t.file_type = 'audio' and t.principal_id is not null
 GROUP BY t.principal_id;
 
-SELECT t.principal_id relateId,	CONCAT('[', GROUP_CONCAT(CONCAT('{"fileUrl":"', t.file_url, '","fileIdentify":"',IFNULL( t.file_identify, '' ),
-				'","coverImageUrl":"', IFNULL( t.cover_image_url, '' ),	'","fileName":"', IFNULL( t.file_name, '' ), '"}')), ']') vrCoverImage
-FROM t_bic_material t
-WHERE t.file_type = 'image'	AND t.principal_id IS NOT NULL AND t.principal_id != ''
-GROUP BY t.principal_id;
-
 SELECT t.principal_id relateId,	CONCAT('[',	GROUP_CONCAT(CONCAT('{"fileUrl":"',	t.file_url,	'","fileIdentify":"', IFNULL( t.file_identify, '' ), '","coverImageUrl":"',
-				IFNULL( t.cover_image_url, '' ), '","fileName":"', IFNULL( t.file_name, '' ), '"}')), ']') videoCoverImage
+				IFNULL( t.cover_image_url, '' ), '","fileName":"', IFNULL( t.file_name, '' ), '"}')), ']') videoResources
 FROM t_bic_material t
 WHERE t.file_type = 'video'	AND t.principal_id IS NOT NULL AND t.principal_id != ''
 GROUP BY t.principal_id;
