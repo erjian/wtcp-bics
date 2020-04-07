8192a0f436cea97642fa1f6b03376e481758900a
linjianwu <linjw824@>
2020-04-07 12:00:45 +0800

    fix(文旅资源管理中心): ES索引添加vr封面图地址和视频封面图地址字段

--- src/main/resources/index/catering/catering.sql
+++ src/main/resources/index/catering/catering.sql
@@ -6,7 +6,7 @@ select t.id relateId,t.id,t.`code`, t.title,t.sub_title subTitle,t.full_spell fu
 t.summary, t.description, t.address,t.`level`,t.score,t.weight,t.cook_style cookStyle,t.feature_tags featureTags,t.web_comment webComment, t.avg_consumption avgConsumption,
 t.open_time openTime,t.traffic_notice trafficNotice,t.repast_notice repastNotice,t.vr_url vrUrl,
 t.latitude, t.longitude,CONCAT(t.latitude,",",t.longitude) geoPoint,t.region,t.region_full_code regionFullCode,t.region_full_name regionFullName,
-t.dept_code deptCode,'' vrCoverImage,'' videoCoverImage,'' images,'' videos,'' audios, '' tags,'' relateTags, '' allTags, if(t.status=9, 1,0) publishStatus
+t.dept_code deptCode,(SELECT REPLACE(s.file_url,'\\','/') vrImage FROM t_bic_material s WHERE TRIM(s.file_identify)='vr' AND s.principal_id = t.id) vrImageUrl,	'' videoResources,'' images,'' videos,'' audios, '' tags,'' relateTags, '' allTags, if(t.status=9, 1,0) publishStatus
 from t_bic_catering t;
 
 
@@ -29,14 +29,8 @@ from t_bic_material t
 where t.file_type = 'audio' and t.principal_id is not null
 GROUP BY t.principal_id;
 
-SELECT t.principal_id relateId,	CONCAT('[', GROUP_CONCAT(CONCAT('{"fileUrl":"', t.file_url, '","fileIdentify":"',IFNULL( t.file_identify, '' ),
-				'","coverImageUrl":"', IFNULL( t.cover_image_url, '' ),	'","fileName":"', IFNULL( t.file_name, '' ), '"}')), ']') vrCoverImage
-FROM t_bic_material t
-WHERE t.file_type = 'image'	AND t.principal_id IS NOT NULL AND t.principal_id != ''
-GROUP BY t.principal_id;
-
 SELECT t.principal_id relateId,	CONCAT('[',	GROUP_CONCAT(CONCAT('{"fileUrl":"',	t.file_url,	'","fileIdentify":"', IFNULL( t.file_identify, '' ), '","coverImageUrl":"',
-				IFNULL( t.cover_image_url, '' ), '","fileName":"', IFNULL( t.file_name, '' ), '"}')), ']') videoCoverImage
+				IFNULL( t.cover_image_url, '' ), '","fileName":"', IFNULL( t.file_name, '' ), '"}')), ']') videoResources
 FROM t_bic_material t
 WHERE t.file_type = 'video'	AND t.principal_id IS NOT NULL AND t.principal_id != ''
 GROUP BY t.principal_id;
