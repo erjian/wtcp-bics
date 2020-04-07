d3b4d7c3d9a023ebe6cb3c83791e9bf389c5f568
linjianwu <linjw824@>
2020-04-07 16:30:14 +0800

    fix(文旅资源管理中心): ES索引添加vr封面图地址和视频封面图地址字段

--- src/main/resources/index/celebrity/celebrity.sql
+++ src/main/resources/index/celebrity/celebrity.sql
@@ -5,7 +5,7 @@
 select t.id relateId,t.id, t.name,t.alias,t.full_spell fullSpell, t.simple_spell simpleSpell,t.category,
 t.summary, t.description, t.slogan,t.sex,t.birth_date birthDate,t.pass_away_date passAwayDate,t.birthplace,t.nation,t.type,t.weight,
 t.behalf_achievement behalfAchievement, t.chief_behalf chiefBehalf,t.story,t.status,t.weight,
-t.dept_code deptCode,(SELECT REPLACE(s.file_url,'\\','/') vrImage FROM t_bic_material s WHERE TRIM(s.file_identify)='vr' AND s.principal_id = t.id) vrImageUrl,	'' videoResources,'' images,'' videos,'' audios, '' tags,'' relateTags, '' allTags, if(t.status=9, 1,0) publishStatus
+t.dept_code deptCode,(SELECT REPLACE(s.file_url,'\\','/') vrImage FROM t_bic_material s WHERE TRIM(s.file_identify)='vr' AND s.principal_id = t.id) vrImageUrl,	null videoResources,'' images,'' videos,'' audios, '' tags,'' relateTags, '' allTags, if(t.status=9, 1,0) publishStatus
 from t_bic_celebrity t;
 
 
